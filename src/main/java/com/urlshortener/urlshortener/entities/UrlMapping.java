4bba4aae68a18c18abfa7bbe5a97fee10fd1225f
AnthoniBortolotto <antho@outlook.com.br>
2025-06-14 15:24:35 -0300

    UrlMapping - added an index to optimize search by the originalUrl

--- src/main/java/com/urlshortener/urlshortener/entities/UrlMapping.java
+++ src/main/java/com/urlshortener/urlshortener/entities/UrlMapping.java
@@ -1,6 +1,7 @@
 package com.urlshortener.urlshortener.entities;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ public class UrlMapping {
     @Id
     private String shortUrl;
     
+    @Indexed(unique = true)
     private String originalUrl;
     private LocalDateTime createdAt;
     private LocalDateTime lastAccessedAt;
